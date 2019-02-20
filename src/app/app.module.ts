95c5dbb8b71439de7c559048b7ede7d4fce656ea
FernandoGalende <fernandogalende@gmail.com>
2019-02-20 10:00:34 +0100

    Implements styles and reducer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+//ngrx
+import { StoreModule} from '@ngrx/store'
+import { todoReducer } from './todo/todo.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+
+//Forms
+import { ReactiveFormsModule } from '@angular/forms'
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
@@ -22,9 +32,15 @@ import { TodoFooterComponent } from './todo/todo-footer/todo-footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    StoreModule.forRoot({ todos: todoReducer}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
--- src/app/todo/todo.reducer.ts
+++ src/app/todo/todo.reducer.ts
@@ -1,9 +1,9 @@
 import * as fromTodo from './todo.actions';
 import { Todo } from './model/todo.model';
 
-const estadoInicial: Todo = [];
+const estadoInicial: Todo[] = [];
 
-export function todoReducer(state, action: fromTodo.Acciones): Todo[] {
+export function todoReducer(state = estadoInicial, action: fromTodo.Acciones): Todo[] {
   switch (action.type){
 
     case fromTodo.AGREGAR_TODO:
